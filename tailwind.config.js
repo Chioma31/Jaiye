4ab4f044d0b8488584f79fdf9b6a76eef1717c15
Chioma <anigboguchioma31@gmail.com>
2023-03-29 10:24:15 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        contrast: 'var(--contrast)',
+        accent: 'var(--accent)',
+      },
+      screens: {
+        mobile: '0px',
+        // => @media () { ... }
+
+        tablet: '1024px',
+        // => @media (min-width: 768px, max:1023px) { ... }
+
+        desktop: '1280px',
+        // => @media (min-width: 1024px and max:1279px) { ... }
+      },
+    },
+
+  },
+  plugins: [],
+}
+
