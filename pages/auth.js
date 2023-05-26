fe7e0b6b834507784a35098c56f78147e7370d18
bagwells <timmyayomidebagwells@outlook.com>
2023-05-26 11:22:07 +0100

    Sign up and Login Page

---
+++ pages/auth.js
@@ -0,0 +1,14 @@
+import Signup, { Login } from "./auth"
+
+
+const Auth= ()=>{
+
+    return(
+        <>
+            <Login/>
+            <Signup/>    
+        </>
+    )
+};
+
+export default Auth;
